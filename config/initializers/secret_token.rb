e278525efe0d2be149ed4e4e239bab04a8fa8610
diogo.engsoftware@gmail.com <diogo.engsoftware@gmail.com>
2012-01-11 01:22:43 -0200

    ruby-on-rails-tutorial-book - introducao ao git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6e0cd66fe3cecf732fd28ce47c20f02768bee48dfe50a51a86e18aba0568b73acb834a592a05d49ad440f5039c038f17ca4bc248bccd5db8132968273d984bee'
